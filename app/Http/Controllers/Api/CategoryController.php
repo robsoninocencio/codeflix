6b004839cd79ed4644a707f553430c98b34d4847
Robson Inocêncio <robson.inocencio@gmail.com>
2021-05-17 00:41:33 +0000

    Resource Categories Get

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        //
+        return Category::all();
     }
 
     public function store(Request $request)
@@ -20,7 +20,7 @@ class CategoryController extends Controller
 
     public function show(Category $category)
     {
-        //
+        return $category;
     }
 
     public function update(Request $request, Category $category)
