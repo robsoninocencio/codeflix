de465e5b5ae151eae26b781255ef9c63803daf1c
Robson Inocêncio <robson.inocencio@gmail.com>
2021-05-29 04:58:16 +0000

    Criando Stubs

---
+++ tests/Feature/Http/Controllers/Api/BasicCrudControllerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Http\Controllers\Api;
+
+use Tests\Stubs\Controllers\CategoryControllerStub;
+use Tests\Stubs\Models\CategoryStub;
+use Tests\TestCase;
+
+class BasicCrudControllerTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        CategoryStub::dropTable();
+        CategoryStub::createTable();
+    }
+
+    protected function tearDown(): void
+    {
+        CategoryStub::dropTable();
+        parent::tearDown();
+    }
+
+    public function testIndex()
+    {
+        /** @var CategoryStub $category */
+        $category = CategoryStub::create(['name' => 'test_name', 'description' => 'test_description']);
+        $controller = new CategoryControllerStub();
+        $result = $controller->index()->toArray();
+        $this->assertEquals([$category->toArray()], $result);
+    }
+}
---
+++ tests/Stubs/Controllers/CategoryControllerStub.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Stubs\Controllers;
+
+use App\Http\Controllers\Api\BasicCrudController;
+use Tests\Stubs\Models\CategoryStub;
+
+class CategoryControllerStub extends BasicCrudController
+{
+    protected function model()
+    {
+        return CategoryStub::class;
+    }
+}
