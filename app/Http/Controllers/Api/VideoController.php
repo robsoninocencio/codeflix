538da0761abb6e6a393595eb4e29a9e7148debca
Robson Inocêncio <robson.inocencio@gmail.com>
2021-06-07 13:25:20 +0000

    Primeiro upload de arquivos

--- app/Http/Controllers/Api/VideoController.php
+++ app/Http/Controllers/Api/VideoController.php
@@ -25,7 +25,7 @@ class VideoController extends BasicCrudController
                 'array',
                 'exists:genres,id,deleted_at,NULL',
             ],
-            "video_file" => "required"
+            "video_file" => 'mimetypes:video/mp4|max:12' //KB
         ];
     }
 
