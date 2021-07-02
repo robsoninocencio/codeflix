8e467e9431720daa705dd30240d4eb248a2acf78
Robson Inocêncio <robson.inocencio@gmail.com>
2021-07-01 23:15:18 -0300

    Criando menu no Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,6 +5,10 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import logo from "../../static/img/logo.png";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 
 const useStyles = makeStyles((theme: Theme) => ({
   toolbar: {
@@ -25,6 +29,18 @@ const Navbar: React.FC = () => {
   return (
     <AppBar>
       <Toolbar className={classes.toolbar}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="open drawer"
+          aria-controls="menu-appbar"
+          aria-haspopup="true"
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu id="menu-appbar" open={false}>
+          <MenuItem>Categorias</MenuItem>
+        </Menu>
         <Typography className={classes.title}>
           <img src={logo} alt="CodeFlix" className={classes.logo} />
         </Typography>
