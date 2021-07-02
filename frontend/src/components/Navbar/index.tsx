eb583400a0a74a2afd870787d802e441aff37b5a
Robson Inocêncio <robson.inocencio@gmail.com>
2021-07-01 21:52:50 -0300

    Estilizando componente Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -2,14 +2,29 @@ import React from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import logo from "../../static/img/logo.png";
 
-// import { Container } from './styles';
+const useStyles = makeStyles({
+  toolbar: {
+    backgroundColor: "#000000",
+  },
+  title: {
+    flexGrow: 1,
+    textAlign: "center",
+  },
+});
 
 const Navbar: React.FC = () => {
+  const classes = useStyles();
   return (
-    <AppBar position="fixed" color="primary">
-      <Toolbar>
-        <Typography variant="h6">NavBar</Typography>
+    <AppBar>
+      <Toolbar className={classes.toolbar}>
+        <Typography className={classes.title}>
+          <img src={logo} alt="CodeFlix" />
+        </Typography>
+        <Button color="inherit">Login</Button>
       </Toolbar>
     </AppBar>
   );
