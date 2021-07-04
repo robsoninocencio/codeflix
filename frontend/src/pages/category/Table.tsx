72b30d206b5a16bd15ded8c30fab4f44f4c0f901
Robson Inocêncio <robson.inocencio@gmail.com>
2021-07-04 00:15:16 -0300

    Configurando Axios

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,5 +1,6 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
+import { httpVideo } from "../../util/http";
 
 const columnsDefinition: MUIDataTableColumn[] = [
   {
@@ -16,15 +17,19 @@ const columnsDefinition: MUIDataTableColumn[] = [
   },
 ];
 
-const data = [
-  { name: "teste1", is_active: true, created_at: "2019-12-12" },
-  { name: "teste2", is_active: false, created_at: "2019-12-13" },
-  { name: "teste3", is_active: true, created_at: "2019-12-14" },
-  { name: "teste4", is_active: false, created_at: "2019-12-15" },
-];
+// const data = [
+//   { name: "teste1", is_active: true, created_at: "2019-12-12" },
+//   { name: "teste2", is_active: false, created_at: "2019-12-13" },
+//   { name: "teste3", is_active: true, created_at: "2019-12-14" },
+//   { name: "teste4", is_active: false, created_at: "2019-12-15" },
+// ];
 
 type Props = {};
 const Table = (props: Props) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    httpVideo.get("categories").then((response) => setData(response.data.data));
+  }, []);
   return (
     <MUIDataTable
       title="Listagem de categorias"
---
+++ frontend/src/util/http/index.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const httpVideo = axios.create({
+  baseURL: process.env.REACT_APP_MICRO_VIDEO_API_URL
+});
