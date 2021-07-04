a1f46b14cc0588f6750b616a8157ec193aac9250
Robson Inocêncio <robson.inocencio@gmail.com>
2021-07-03 23:23:33 -0300

    Primeiro Contato Com Mui-Datatables

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,7 +1,37 @@
 import * as React from "react";
+import MUIDataTable, { MUIDataTableColumn } from "mui-datatables";
+
+const columnsDefinition: MUIDataTableColumn[] = [
+  {
+    name: "name",
+    label: "Nome",
+  },
+  {
+    name: "is_active",
+    label: "Ativo?",
+  },
+  {
+    name: "created_at",
+    label: "Criado em",
+  },
+];
+
+const data = [
+  { name: "teste1", is_active: true, created_at: "2019-12-12" },
+  { name: "teste2", is_active: false, created_at: "2019-12-13" },
+  { name: "teste3", is_active: true, created_at: "2019-12-14" },
+  { name: "teste4", is_active: false, created_at: "2019-12-15" },
+];
+
 type Props = {};
 const Table = (props: Props) => {
-  return <div> Table </div>;
+  return (
+    <MUIDataTable
+      title="Listagem de categorias"
+      columns={columnsDefinition}
+      data={data}
+    />
+  );
 };
 
 export default Table;
