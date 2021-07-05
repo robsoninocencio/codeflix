011dfa5e813a7107deefd1c2b4fc7930bfd5b140
Robson Inocêncio <robson.inocencio@gmail.com>
2021-07-05 12:17:41 -0300

    Cadastrando categorias

--- frontend/src/pages/category/Form.tsx
+++ frontend/src/pages/category/Form.tsx
@@ -5,6 +5,7 @@ import Button, { ButtonProps } from "@material-ui/core/Button";
 import Checkbox from "@material-ui/core/Checkbox";
 import TextField from "@material-ui/core/TextField";
 import { useForm } from "react-hook-form";
+import categoryHttp from "../../util/http/category-http";
 
 const useStyles = makeStyles((theme: Theme) => {
   return {
@@ -22,10 +23,19 @@ export const Form = () => {
     className: classes.submit,
   };
 
-  const { register, getValues } = useForm();
+  const { register, handleSubmit, getValues } = useForm({
+    defaultValues: {
+      is_active: true,
+    },
+  });
+
+  function onSubmit(formData, event) {
+    console.log("event = ", event);
+    categoryHttp.create(formData).then((response) => console.log(response));
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <TextField
         name="name"
         label="Nome"
@@ -43,10 +53,10 @@ export const Form = () => {
         margin="normal"
         inputRef={register}
       />
-      <Checkbox name="is_active" inputRef={register} />
+      <Checkbox name="is_active" inputRef={register} defaultChecked />
       Ativo?
       <Box dir="rtl">
-        <Button {...buttonProps} onClick={() => console.log(getValues())}>
+        <Button {...buttonProps} onClick={() => onSubmit(getValues(), null)}>
           Salvar
         </Button>
         <Button {...buttonProps} type="submit">
