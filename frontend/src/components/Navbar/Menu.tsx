bba6a0f5398fffd3d1b1c466b6f6958cedcd8e74
Robson Inocêncio <robson.inocencio@gmail.com>
2021-08-02 14:22:43 -0300

    ajuste na apresentação do Menu

--- frontend/src/components/Navbar/Menu.tsx
+++ frontend/src/components/Navbar/Menu.tsx
@@ -18,9 +18,12 @@ const menuRoutes = routes.filter((route) =>
 
 const Menu: React.FC = () => {
   const [anchorEl, setAnchorEl] = useState(null);
-  const handleOpen = (event: any) => setAnchorEl(event.currentTarget);
   const open = Boolean(anchorEl);
+
+  const handleOpen = (event: any) => setAnchorEl(event.currentTarget);
+
   const handleClose = () => setAnchorEl(null);
+
   return (
     <>
       <IconButton
@@ -40,7 +43,7 @@ const Menu: React.FC = () => {
         anchorEl={anchorEl}
         onClose={handleClose}
         anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
-        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        transformOrigin={{ vertical: "top", horizontal: "left" }}
         getContentAnchorEl={null}
       >
         {Object.keys(listRoutes).map((routeName, key) => {
